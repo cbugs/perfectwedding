4f3ec2e620c54dde47a52425a0d9fcb5630b56cd
Pajaani Soobrayen <Pajaani Soobrayen>
2017-06-17 20:00:09 +0400

    init

--- www/web/deploy.php
+++ www/web/deploy.php
@@ -5,7 +5,7 @@ foreach ($output as $o) {
     echo $o . '<br/>';
 }
 echo "End: Pull code>";
-exec('php ~/perfectwedding/bin/console cache:clear', $output2);
-foreach ($output2 as $o) {
-    echo $o . '<br/>';
-}
+// exec('php ~/perfectwedding/bin/console cache:clear', $output2);
+// foreach ($output2 as $o) {
+//     echo $o . '<br/>';
+// }
