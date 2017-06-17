9e8d159de3ffc36e12ff1860e015c28bd5815004
Pajaani Soobrayen <Pajaani Soobrayen>
2017-06-17 11:23:06 +0400

    auto cache clear

--- web/deploy.php
+++ web/deploy.php
@@ -5,3 +5,7 @@ foreach ($output as $o) {
     echo $o . '<br/>';
 }
 echo "End: Pull code>";
+exec('php ~/perfectwedding/bin/console cache:clear', $output2);
+foreach ($output2 as $o) {
+    echo $o . '<br/>';
+}
