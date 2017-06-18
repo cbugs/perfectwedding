e40bd6a032ce2926dce330e3fd19c4298cb27ac5
Pajaani Soobrayen <Pajaani Soobrayen>
2017-06-18 17:05:23 +0400

    d2

--- www/src/WeddingBundle/Controller/ListingController.php
+++ www/src/WeddingBundle/Controller/ListingController.php
@@ -11,7 +11,14 @@ class ListingController extends Controller
   
   public function searchAction(Request $request)
   {
-    $apiData = BaseController::callAPI('GET','/search_product?location_tid='.$_GET['location'].'&category_tid='.$_GET['category']);
+    $url = '/search_product?'
+    if(isset($_GET['location']) && !empty($_GET['location'])){
+      $url .= 'location_tid='.$_GET['location'].'&';
+    }
+    if(isset($_GET['category']) && !empty($_GET['category'])){
+      $url .= 'category_tid='.$_GET['category'];
+    }
+    $apiData = BaseController::callAPI('GET',$url);
     $results = json_decode($apiData, true);//var_dump('/search_product?location_tid='.$_GET['location'].'&category_tid='.$_GET['category']);exit;
     return $this->render('WeddingBundle:Listings:search.html.twig', array(
       'category' => 'All',
