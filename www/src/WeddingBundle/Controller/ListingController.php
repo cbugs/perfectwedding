83562965570cbdc43d14a4749fd4938e8c466ecb
Pajaani <soobrayenp@proximity.bbdo.fr>
2017-06-18 14:16:59 +0400

    d2

--- www/src/WeddingBundle/Controller/ListingController.php
+++ www/src/WeddingBundle/Controller/ListingController.php
@@ -4,6 +4,7 @@ namespace WeddingBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class ListingController extends Controller
 {
