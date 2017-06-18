cabf9e51c2cff4e292e84c97aa415e9d9c9b97a6
Pajaani Soobrayen <Pajaani Soobrayen>
2017-06-18 11:00:02 +0400

    google places api module

--- supplier/sites/default/settings.php
+++ supplier/sites/default/settings.php
@@ -320,7 +320,7 @@ $settings['hash_salt'] = 'uHCwcqoRliOydUBq4aePsmhlE167xrl-hyk2trPQ4yDSiktt6VQ-0V
  * After finishing the upgrade, be sure to open this file again and change the
  * TRUE back to a FALSE!
  */
-$settings['update_free_access'] = FALSE;
+$settings['update_free_access'] = TRUE;
 
 /**
  * External access proxy settings:
