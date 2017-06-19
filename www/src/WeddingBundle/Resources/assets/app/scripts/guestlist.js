e6a8c13ccfbeafe370a91748ed31cf23616e7840
Pajaani Soobrayen <Pajaani Soobrayen>
2017-06-20 01:09:17 +0400

    d3

--- www/src/WeddingBundle/Resources/assets/app/scripts/guestlist.js
+++ www/src/WeddingBundle/Resources/assets/app/scripts/guestlist.js
@@ -173,5 +173,78 @@
 
     $('#list').click(function(event){event.preventDefault();$('#products .item').addClass('list-group-item');});
     $('#grid').click(function(event){event.preventDefault();$('#products .item').removeClass('list-group-item');$('#products .item').addClass('grid-group-item');});
-
+    var start_drag = function(){
+    $( "#sortable, .snap_target" ).sortable({
+      revert: true
+    });
+    $( "#draggable, #sortable li, .snap_target li" ).draggable({
+      appendTo: "body",
+      connectToSortable: ".snap_target",
+      revert: "invalid",
+      cursor: "move"
+    });
+  $('.snap_target').droppable(
+    {accept: "li"},{
+     hoverClass: "hover", 
+     drop: function( event, ui ) {
+       var count = $(this).find('li').length - 1;
+       $('.snap_target').removeClass('hover');
+       if(count > 0){ 
+         $( this )
+            .addClass( "highlight" )
+            .find( "p" )
+          .html( count );
+         start_drag();
+       }else{
+         $( this )
+            .addClass( "highlight" )
+            .find( "p" )
+          .html( '0' );
+       }
+     }
+    });
+    };
+    
+  var table = $('.sortable'),
+      z = 1;
+  
+  $('#table').on("click", function(){
+      z++;
+      $('#tables').append('<ul id="table_'+z+'" class="snap_target guests"><p></p><span class="table_number" contenteditable="true">'+z+'</span></ul>');
+      start_drag();
+  });
+  
+  
+  $('#fix').on("click", function(){
+    start_drag();
+  });
+    
+  
+  $("#add_guests").on("click", function(){
+    var temp = $("#guest_names").val();
+    var guests = [];
+    guests = temp.split(",");
+    
+    for (var i = 0; i < guests.length; i++) {
+      if (guests[i]!==" " ){
+        $("#sortable").append("<li><div contenteditable='true'>"+guests[i]+"</div><span>x</span></li>");
+      }
+    }
+    start_drag();
+  });
+  
+  $("#export").on("click", function(){
+  var export_val = $('#tables').html(); 
+  $("#export_text").val(export_val);
+    $("#tables ul").each(function(){
+      var table_no = "\(" + $(this).find(".table_number").text()+"\)",
+      guest_na = "" + $(this).find('li div').text();
+      console.log(table_no + guest_na[0]); 
+     });
+   
+  });
+  $('body').on("click", "li span", function(){
+    $(this).parent().remove();
+  })
 } ) ( jQuery );
+
