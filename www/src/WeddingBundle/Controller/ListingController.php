cf918f1be72001b928ffaa8a6c9f6a8a18a6a213
Pajaani Soobrayen <Pajaani Soobrayen>
2017-06-19 21:46:08 +0400

    d3

--- www/src/WeddingBundle/Controller/ListingController.php
+++ www/src/WeddingBundle/Controller/ListingController.php
@@ -22,11 +22,13 @@ class ListingController extends BaseController
     $apiData = BaseController::callAPI('GET',$url);
     $results = json_decode($apiData, true);//var_dump('/search_product?location_tid='.$_GET['location'].'&category_tid='.$_GET['category']);exit;
     
-    $em = $this->getDoctrine()->getManager();
-    $userFavourites = $em->getRepository('WeddingBundle:Product\Favourite')->findBy(array('active' => 1,'user_id'=>$this->getCurrentUser()->getId()));
     $favouriteProducts = array();
-    foreach($userFavourites as $userFavourite) {
-        $favouriteProducts[] = $userFavourite->getProductId();
+    if($this->getCurrentUser() != null){
+      $em = $this->getDoctrine()->getManager();
+      $userFavourites = $em->getRepository('WeddingBundle:Product\Favourite')->findBy(array('active' => 1,'user_id'=>$this->getCurrentUser()->getId()));
+      foreach($userFavourites as $userFavourite) {
+          $favouriteProducts[] = $userFavourite->getProductId();
+      }
     }
     
     return $this->render('WeddingBundle:Listings:search.html.twig', array(
