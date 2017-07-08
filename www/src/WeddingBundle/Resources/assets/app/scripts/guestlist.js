5c2d6dcd9775f2c45de8f973b5fb18125147b65f
Pajaani Soobrayen <Pajaani Soobrayen>
2017-07-08 20:29:17 +0400

    event

--- www/src/WeddingBundle/Resources/assets/app/scripts/guestlist.js
+++ www/src/WeddingBundle/Resources/assets/app/scripts/guestlist.js
@@ -18,6 +18,32 @@ var todayDate = moment().startOf('day');
 		editable: true,
 		eventLimit: true, // allow "more" link when too many events
 		navLinks: true,
+
+            droppable: true, // this allows things to be dropped onto the calendar
+            dragRevertDuration: 0,
+            drop: function() {
+                // is the "remove after drop" checkbox checked?
+                if ($('#drop-remove').is(':checked')) {
+                    // if so, remove the element from the "Draggable Events" list
+                    $(this).remove();
+                }
+            },
+            eventDragStop: function( event, jsEvent, ui, view ) {
+                
+                if(isEventOverDiv(jsEvent.clientX, jsEvent.clientY)) {
+                    $('#calendar').fullCalendar('removeEvents', event._id);
+                    var el = $( "<div class='fc-event'>" ).appendTo( '#external-events-listing' ).text( event.title );
+                    el.draggable({
+                      zIndex: 999,
+                      revert: true, 
+                      revertDuration: 0 
+                    });
+                    el.data('event', { title: event.title, id :event.id, stick: true });
+                }
+            },
+
+
+
 		events: [
 			{
 				title: 'All Day Event',
@@ -77,6 +103,34 @@ var todayDate = moment().startOf('day');
 	});
 
 setTimeout(function(){$('#calendar').fullCalendar('option', 'aspectRatio', 1.8);},500);
+
+
+
+        $('#external-events .fc-event').each(function() {
+
+            // store data so the calendar knows to render an event upon drop
+            $(this).data('event', {
+                title: $.trim($(this).text()), // use the element's text as the event title
+                stick: true // maintain when user navigates (see docs on the renderEvent method)
+            });
+
+            // make the event draggable using jQuery UI
+            $(this).draggable({
+                zIndex: 999,
+                revert: true,      // will cause the event to go back to its
+                revertDuration: 0  //  original position after the drag
+            });
+
+        });
+
+
+
+
+
+
+
+
+
     var navListItems = $('ul.setup-panel li a'),
         allWells = $('.setup-content');
 
