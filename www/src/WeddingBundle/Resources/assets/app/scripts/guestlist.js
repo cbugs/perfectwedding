954adc2c974cc9bf63c2611f9be929ffcdc40a37
Pajaani Soobrayen <Pajaani Soobrayen>
2017-07-08 20:15:42 +0400

    event

--- www/src/WeddingBundle/Resources/assets/app/scripts/guestlist.js
+++ www/src/WeddingBundle/Resources/assets/app/scripts/guestlist.js
@@ -18,6 +18,9 @@ var todayDate = moment().startOf('day');
 		editable: true,
 		eventLimit: true, // allow "more" link when too many events
 		navLinks: true,
+        eventAfterRender: function(event, element) {
+            $('#calendar').fullCalendar('option', 'aspectRatio', 1.8);
+        },
 		events: [
 			{
 				title: 'All Day Event',
@@ -76,7 +79,7 @@ var todayDate = moment().startOf('day');
 		]
 	});
 
-$('#calendar').fullCalendar('option', 'aspectRatio', 1.8);
+
     var navListItems = $('ul.setup-panel li a'),
         allWells = $('.setup-content');
 
