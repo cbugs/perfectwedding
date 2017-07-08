74de6d4493c8c3d4f90e80bb8e365d0bed61be7f
Pajaani Soobrayen <Pajaani Soobrayen>
2017-07-08 19:29:02 +0400

    event

--- www/src/WeddingBundle/Resources/assets/app/scripts/vendor.js
+++ www/src/WeddingBundle/Resources/assets/app/scripts/vendor.js
@@ -5,6 +5,7 @@ require("slick-carousel");
 require('jquery-circle-progress');
 require('./vendors/awesomplete');
 require('jquery-ui-dist/jquery-ui');
-// require("fullcalendar");
+require("moment");
+require("fullcalendar");
 // require('lazyloadxt');
 //<script src="jquery-circle-progress/dist/circle-progress.js"></script>
