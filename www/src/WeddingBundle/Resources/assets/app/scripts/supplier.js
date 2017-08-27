e2a6fbc5084ad39f5754be02eb96dc2400859909
Pajaani Soobrayen <Pajaani Soobrayen>
2017-08-27 09:04:19 +0400

    supplier

--- www/src/WeddingBundle/Resources/assets/app/scripts/supplier.js
+++ www/src/WeddingBundle/Resources/assets/app/scripts/supplier.js
@@ -7,5 +7,21 @@ $(document).ready(function() {
     });
 
   
+ $('.slider-supplier-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-supplier-nav').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true
+});
+
 
 });
