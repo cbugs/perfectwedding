5a5a46bd43c3e1228f1abdb9e7888c2a3a46f9b7
Pajaani Soobrayen <Pajaani Soobrayen>
2017-08-27 09:18:26 +0400

    supplier

--- www/src/WeddingBundle/Resources/assets/app/scripts/vendor.js
+++ www/src/WeddingBundle/Resources/assets/app/scripts/vendor.js
@@ -7,5 +7,6 @@ require('./vendors/awesomplete');
 require('jquery-ui-dist/jquery-ui');
 // var moment = require('moment');
 require("fullcalendar");
+require("slick-carousel");
 // require('lazyloadxt');
 //<script src="jquery-circle-progress/dist/circle-progress.js"></script>
