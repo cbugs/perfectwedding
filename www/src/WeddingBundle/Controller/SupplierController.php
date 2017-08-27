5afcb67dab8ffabfff1c71100e61ddaa5769bc8f
Pajaani Soobrayen <Pajaani Soobrayen>
2017-08-27 08:44:41 +0400

    supplier

---
+++ www/src/WeddingBundle/Controller/SupplierController.php
@@ -0,0 +1,31 @@
+<?php
+// src/WeddingBundle/Controller/RegisterController.php
+namespace WeddingBundle\Controller;
+
+use WeddingBundle\Form\User\RegisterForm;
+use WeddingBundle\Form\User\CoupleForm;
+use WeddingBundle\Form\User\SupplierForm;
+use WeddingBundle\Entity\User\User;
+use WeddingBundle\Entity\User\Couple;
+use WeddingBundle\Entity\User\Confirmation;
+use WeddingBundle\Entity\User\Supplier;
+use WeddingBundle\Entity\SecurityToken;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use WeddingBundle\Controller\BaseController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\HttpFoundation\Cookie;
+
+class SupplierController extends BaseController
+{
+
+    public function indexAction($id)
+    {
+        return $this->render(
+            'WeddingBundle:Supplier:index.html.twig'
+            //,array('form' => $form->createView())
+        );
+    }
+
+}
