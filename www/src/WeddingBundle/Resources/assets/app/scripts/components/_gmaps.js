39c1fb63c5cbfc67fd0e507f1ff20a9acf96a8e3
Pajaani Soobrayen <Pajaani Soobrayen>
2017-09-09 09:58:55 +0400

    build

--- www/src/WeddingBundle/Resources/assets/app/scripts/components/_gmaps.js
+++ www/src/WeddingBundle/Resources/assets/app/scripts/components/_gmaps.js
@@ -60,29 +60,29 @@
 //     });
 // }
 
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener('DOMContentLoaded', () => {
 
-        var myLatlng = new google.maps.LatLng(40.713956, -74.006653);
-        var myOptions = {
-            zoom: 8,
-            center: myLatlng,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-        var marker = new google.maps.Marker({
-            draggable: true,
-            position: myLatlng,
-            map: map,
-            title: "Your location"
-        });
-        google.maps.event.addListener(marker, 'dragend', function (event) {
-            document.getElementById("lat").value = event.latLng.lat();
-            document.getElementById("long").value = event.latLng.lng();
-            infoWindow.open(map, marker);
-        });
+//         var myLatlng = new google.maps.LatLng(40.713956, -74.006653);
+//         var myOptions = {
+//             zoom: 8,
+//             center: myLatlng,
+//             mapTypeId: google.maps.MapTypeId.ROADMAP
+//         };
+//         var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+//         var marker = new google.maps.Marker({
+//             draggable: true,
+//             position: myLatlng,
+//             map: map,
+//             title: "Your location"
+//         });
+//         google.maps.event.addListener(marker, 'dragend', function (event) {
+//             document.getElementById("lat").value = event.latLng.lat();
+//             document.getElementById("long").value = event.latLng.lng();
+//             infoWindow.open(map, marker);
+//         });
     
     
 
 
 
-});
+// });
