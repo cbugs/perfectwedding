592b629625662deff61cb74165baf7e783602070
Pajaani Soobrayen <Pajaani Soobrayen>
2017-09-09 10:04:31 +0400

    build

--- www/web/bundles/bo/js/sb-admin.js
+++ www/web/bundles/bo/js/sb-admin.js
@@ -55,6 +55,51 @@
     $('#dataTable').DataTable();
   });
 
+
+
+
+
+
+
+console.log("maping");
+
+
+        var myLatlng = new google.maps.LatLng(40.713956, -74.006653);
+        var myOptions = {
+            zoom: 8,
+            center: myLatlng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+        var marker = new google.maps.Marker({
+            draggable: true,
+            position: myLatlng,
+            map: map,
+            title: "Your location"
+        });
+        google.maps.event.addListener(marker, 'dragend', function (event) {
+            document.getElementById("lat").value = event.latLng.lat();
+            document.getElementById("long").value = event.latLng.lng();
+            infoWindow.open(map, marker);
+        });
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })(jQuery); // End of use strict
 
 // Chart.js scripts
